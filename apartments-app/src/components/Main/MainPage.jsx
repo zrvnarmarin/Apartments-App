9957eb894c6c45ef5e1d4097e27409afa2dcdb01
zrvnarmarin <76marin76@gmail.com>
2023-01-17 20:32:57 +0100

    navbar finished

---
+++ apartments-app/src/components/Main/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const MainPage = () => {
+    
+
+  return (
+    <main className='w-screen h-screen bg-gray-100 font-poppins'>
+        <Navbar />
+    </main>
+  )
+}
+
+export default MainPage
