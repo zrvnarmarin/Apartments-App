2631ed91ee2b910376ed2af36b5f146c17c1f7a0
zrvnarmarin <76marin76@gmail.com>
2023-01-18 23:46:22 +0100

    apartments added

--- apartments-app/src/components/Main/Apartments.jsx
+++ apartments-app/src/components/Main/Apartments.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Apartments = () => {
+
+  const apartments = [
+    { id: 1, status: 'reserved', title: '2-room suite moonlightly', city: 'Zadar', rooms: 2, price: 12.45},
+    { id: 2, status: 'occupied', title: 'sunsihine rooms', city: 'Split', rooms: 3, price: 23.45},
+    { id: 3, status: 'free', title: '4-room suite moonlightly', city: 'Sibenik', rooms: 1, price: 60.45},
+
+  ]
+  
   return (
     <div>
-      jhodfuihvb
+      <table>
+
+      </table>
     </div>
   )
 }
