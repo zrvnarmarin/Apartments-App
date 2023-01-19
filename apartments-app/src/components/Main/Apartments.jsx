67230cd77812883945588d28ef5910cac83e7352
zrvnarmarin <76marin76@gmail.com>
2023-01-19 20:33:24 +0100

    firebase connection

--- apartments-app/src/components/Main/Apartments.jsx
+++ apartments-app/src/components/Main/Apartments.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AddNewApartment from './AddNewApartment'
-import { Link, Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import ArrowDown from '../../assets/DownArrow.svg'
 
 const Apartments = ({ isModalOpen, onModalOpen, onModalClose }) => {
 
@@ -25,26 +26,33 @@ const Apartments = ({ isModalOpen, onModalOpen, onModalClose }) => {
                 <th className='py-4 px-6'>Rooms</th>
                 <th className='py-4 px-6'>Price</th>
                 <th className='py-4 px-6'>Image</th>
+                <th className='py-4 px-6'></th>
               </tr>
             </thead>
             <tbody>
               {apartments.map(apartment =>
                 <tr key={apartment.id} className='border-b-[#23272f] border-b-[1px] hover:bg-[#4c5a70] duration-100'>
-                  <td className='py-2 px-6'>{apartment.id}</td>
-                  <td className='py-2 px-6'>{apartment.status}</td>
-                  <td className='py-2 px-6'>{apartment.reservedBy}</td>
-                  <td className='py-2 px-6'>{apartment.title}</td>
-                  <td className='py-2 px-6'>{apartment.city}</td>
-                  <td className='py-2 px-6'>{apartment.rooms}</td>
-                  <td className='py-2 px-6'>{apartment.price}</td>
-                  <td className='py-2 px-6'>
+                  <td className='py-4 px-6'>{apartment.id}</td>
+                  <td className='py-4 px-6'>{apartment.status}</td>
+                  <td className='py-4 px-6'>{apartment.reservedBy}</td>
+                  <td className='py-4 px-6'>{apartment.title}</td>
+                  <td className='py-4 px-6'>{apartment.city}</td>
+                  <td className='py-4 px-6'>{apartment.rooms}</td>
+                  <td className='py-4 px-6'>{apartment.price}</td>
+                  <td className='py-4 px-6'>
                     <img
                       className='hover:scale-[600%] duration-200'
                       width='30'
                       height='30'
                       src='https://www.phillyaptrentals.com/wp-content/uploads/2020/12/apartment-building-what-makes-good-apartment-building-scaled.jpg' />
                   </td>
-      
+                  <td>
+                    <button
+                      className='px-6 py-1 rounded-2xl font-semibold text-md text-[#f6f7f9] flex items-center justify-center'
+                    >
+                      <img height="20" width="20" src={ArrowDown} alt="Down arrow icon" />
+                    </button>
+                  </td>
                 </tr>
               )}
             </tbody>
