5e54543aedb32a8ca118876df516a929370e03b6
zrvnarmarin <76marin76@gmail.com>
2023-01-19 01:28:00 +0100

    importing icons for apartment availability status

--- apartments-app/src/components/Main/Apartments.jsx
+++ apartments-app/src/components/Main/Apartments.jsx
@@ -8,13 +8,22 @@ const Apartments = () => {
     { id: 3, status: 'free', reservedBy: 'marin', title: '4-room suite moonlightly', city: 'Sibenik', rooms: 1, price: 60.45}
   ]
 
+  const statusUnicode = {
+    symbol: '&#9711;',
+    colors: [
+      {reserved: 'bg-red-600'},
+      {occupied: 'bg-yellow-600'},
+      {free: 'bg-green-600'}
+    ]
+  }
+
   return (
     <div className='flex flex-col gap-16 justify-center pt-36 px-36'>
-        <h1 className='border-b-[#23272f] border-b-[1px] pb-4 italic text-4xl font-normal text-[#f6f7f9] text-left'>Apartments</h1>
+        <h1 className='border-b-[#374151] border-b-[1px] pb-4 italic text-4xl font-normal text-[#f6f7f9] text-left'>Apartments</h1>
         <table className='bg-[#374151] text-[#f6f7f9] rounded-md'>
           <thead>
             <tr className='border-b-[#23272f] border-b-[1px]'>
-              <th className='py-6 px-6'>#</th>
+              <th className='py-6 px-6 bg-[#149eca]'>#</th>
               <th className='py-4 px-6'>Status</th>
               <th className='py-4 px-6'>Reserved By</th>
               <th className='py-4 px-6'>Title</th>
@@ -35,7 +44,13 @@ const Apartments = () => {
                 <td className='py-2 px-6'>{apartment.city}</td>
                 <td className='py-2 px-6'>{apartment.rooms}</td>
                 <td className='py-2 px-6'>{apartment.price}</td>
-                <td className='py-2 px-6'><img width='30' height='30' src='https://www.phillyaptrentals.com/wp-content/uploads/2020/12/apartment-building-what-makes-good-apartment-building-scaled.jpg' /></td>
+                <td className='py-2 px-6'>
+                  <img 
+                    className='hover:scale-[600%] duration-200'
+                    width='30' 
+                    height='30' 
+                    src='https://www.phillyaptrentals.com/wp-content/uploads/2020/12/apartment-building-what-makes-good-apartment-building-scaled.jpg' />
+                </td>
                 <td className='py-2 px-6'>
                   <button className='px-6 py-1 rounded-2xl font-semibold text-md text-[#f6f7f9] bg-[#149eca]'>Open</button>
                 </td>
