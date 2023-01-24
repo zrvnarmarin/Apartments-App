54a39e90ddb0e6e836b0fb5f5702c778a3b6b9b7
zrvnarmarin <76marin76@gmail.com>
2023-01-24 02:16:39 +0100

    adding validity to login info

--- apartments-app/src/App.jsx
+++ apartments-app/src/App.jsx
@@ -52,13 +52,14 @@
 
 import React, { useState, useReducer, useEffect } from "react";
 import useHttpRequest from "./hooks/UseHttpRequest";
+import LoginPage from './components/Login/LoginPage.jsx'
+import { validateEmail } from "./utils/utilityFunctions";
 
 const ACTIONS = {
   ADD_TITLE: 'add title',
   ADD_ROOMS: 'add rooms',
   ADD_ADDRESS: 'add address',
   RESET_ALL_INPUTS: 'reset all inputs',
-  ADD_APARTMENTS: 'add apartments'
 }
 
 const apartmentReducer = (state, action) => {
@@ -84,9 +85,6 @@ const apartmentReducer = (state, action) => {
     case ACTIONS.RESET_ALL_INPUTS: {
       return { title: '', rooms: '', address: ''}
     }
-    case ACTIONS.ADD_APARTMENTS: {
-      return { ...state, apartments: action.payload }
-    }
   }
 }
 
@@ -107,7 +105,6 @@ const App = () => {
     }
 
     setApartments(loadedApartments)
-    // addAllApartments(loadedApartments)
   }
   const { error, isLoading, getData, postData } = useHttpRequest(link, transformApartments)
 
@@ -115,8 +112,7 @@ const App = () => {
   const addressChangeHandler = e => dispatch({ type: ACTIONS.ADD_ADDRESS, payload: e.target.value })
   const roomsChangeHandler = e => dispatch({ type: ACTIONS.ADD_ROOMS, payload: e.target.value })
   const resetAllInputs = () => dispatch({ type: ACTIONS.RESET_ALL_INPUTS })
-  const addAllApartments = (loadedApartments) => dispatch({ type: ACTIONS.ADD_APARTMENTS, payload: loadedApartments })
-  const getAllApartments = async () => getData()
+  const getAllApartments = (async () => getData())
   const addNewApartment = async (newApartment) => postData(newApartment)
 
   const formSubmitHandler = e => {
@@ -133,31 +129,109 @@ const App = () => {
     resetAllInputs()
   }
 
-  useEffect(() => {
-    getAllApartments()
-  }, [getAllApartments])
+  // useEffect(() => {
+  //   getAllApartments()
+  // }, [getAllApartments])
+
+  //OVO JE SECTION 16 za LOGIN PAGE I OVAKO ZA VJEZBU//
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const enteredNameIsValid = enteredName.trim() !== '' 
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+  const nameChangeHandler = e => {
+    setEnteredName(e.target.value)
+  }
+  const nameBlurHandler = e => {
+    setEnteredNameTouched(true)
+  }
+
+  const [enteredEmail, setEnteredEmail] = useState('')
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false)
+
+  const enteredEmailIsValid = validateEmail(enteredEmail) && enteredEmail.trim() !== ''
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched
+
+  const emailChangeHandler = e => setEnteredEmail(e.target.value)
+  const emailBlurHandler = e => setEnteredEmailTouched(true)
+
+
+  let formIsValid = false
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true
+  } 
+
+  const submitHandler = e => {
+    e.preventDefault()
+
+    setEnteredNameTouched(true)
+    setEnteredEmailTouched(true)
+
+    if (!enteredNameIsValid && !enteredEmailIsValid) return
+
+    console.log('form submited!')
+    setEnteredName('')
+    setEnteredNameTouched(false)
+
+    setEnteredEmail('')
+    setEnteredEmailTouched(false)
+  }
 
   return (
-    <div>
-      { !isLoading && error && <p>{error}</p> }
-      { isLoading && <p>Loading...</p>}
-      <p className="bg-blue-300 text-white text-4xl">GET:</p>
-      {apartments.map(apartment => 
-        <div key={apartment.id} className="border-2 border-black p-4 rounded-xl">
-          <p>ID: {apartment.id}</p>
-          <p>TITLE: {apartment.title}</p>
-          <p>ROOMS: {apartment.rooms}</p>
-          <p>ADDRESS: {apartment.address}</p>
-        </div>  
-      )}
-      <p className="bg-red-300 text-white text-4xl">POST:</p>
-      <form onSubmit={formSubmitHandler} className='bg-green-500 text-black p-10 flex flex-col gap-5 w-1/2'>
-        <input value={state.title} onChange={titleChangeHandler} type='text' placeholder="title" />
-        <input value={state.rooms} onChange={roomsChangeHandler} type='text' placeholder="rooms" />
-        <input value={state.address} onChange={addressChangeHandler} type='text' placeholder="address" />
-        <button className='text-white px-6 py-1 rounded-2xl text-xl bg-rose-600' >Submit</button>
-      </form>
-    </div>
+    // <div>
+    //   { !isLoading && error && <p>{error}</p> }
+    //   { isLoading &&  <p>Loading...</p>}
+    //   <p className="bg-blue-300 text-white text-4xl">GET:</p>
+
+    //   {apartments.map(apartment => 
+    //     <div key={apartment.id} className="border-2 border-black p-4 rounded-xl">
+    //       <p>ID: {apartment.id}</p>
+    //       <p>TITLE: {apartment.title}</p>
+    //       <p>ROOMS: {apartment.rooms}</p>
+    //       <p>ADDRESS: {apartment.address}</p>
+    //     </div>  
+    //   )}
+    //   <p className="bg-red-300 text-white text-4xl">POST:</p>
+
+    //   <form onSubmit={formSubmitHandler} className='bg-green-500 text-black p-10 flex flex-col gap-5 w-1/2'>
+    //     <input value={state.title} onChange={titleChangeHandler} type='text' placeholder="title" />
+    //     <input value={state.rooms} onChange={roomsChangeHandler} type='text' placeholder="rooms" />
+    //     <input value={state.address} onChange={addressChangeHandler} type='text' placeholder="address" />
+    //     <button className='text-white px-6 py-1 rounded-2xl text-xl bg-rose-600' >Submit</button>
+    //   </form>
+    //   <p className="bg-blue-300 text-white text-4xl">FORM VALIDATION:</p>
+    // </div>
+    <form 
+      onSubmit={submitHandler} 
+      className="border-[1px] border-black p-4 rounded-xl flex flex-col bg-blue-100">
+      <div className="flex flex-col gap-4">
+        <label className="font-medium font-poppins text-xl">Your name</label>
+        <input 
+          onChange={nameChangeHandler} 
+          onBlur={nameBlurHandler}
+          value={enteredName} 
+          type="text" 
+          className="p-2 rounded-lg w-1/2 border-2 border-black"
+        />
+        { nameInputIsInvalid && <p className="text-red-600 font-medium text-md">Name must not be empty!</p> }
+      </div>
+      <div className="flex flex-col gap-4">
+        <label className="font-medium font-poppins text-xl">Your email</label>
+        <input 
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+          type="email" 
+          className="p-2 rounded-lg w-1/2 border-2 border-black"
+        />
+        { emailInputIsInvalid && <p className="text-red-600 font-medium text-md">Enter correct email!</p> }
+      </div>
+      <div className="flex justify-end items-center">
+      <button disabled={!formIsValid} className="px-6 py-1 rounded-xl text-2xl text-white bg-orange-300">Submit</button>
+      </div>
+  </form>
   )
 }
 
