5bfe747a2262eba10e5d3e55bcd89f0c1a87ab95
zrvnarmarin <76marin76@gmail.com>
2023-01-28 23:46:29 +0100

    tasks usage

--- apartments-app/src/App.jsx
+++ apartments-app/src/App.jsx
@@ -52,160 +52,106 @@
 
 import React, { useState, useEffect, useCallback, useRef } from "react";
 
-// function App() {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [tasks, setTasks] = useState([]);
-
-//   const fetchTasks = async (taskText) => {
-//     setIsLoading(true);
-//     setError(null);
-//     try {
-//       const response = await fetch(
-//         'https://apartments-app-6a66f-default-rtdb.firebaseio.com/tasks.json'
-//       );
-
-//       if (!response.ok) {
-//         throw new Error('Request failed!');
-//       }
-
-//       const data = await response.json();
-
-//       const loadedTasks = [];
-
-//       for (const taskKey in data) {
-//         loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-//       }
-
-//       setTasks(loadedTasks);
-//     } catch (err) {
-//       setError(err.message || 'Something went wrong!');
-//     }
-//     setIsLoading(false);
-//   };
-
-//   useEffect(() => {
-//     fetchTasks();
-//   }, []);
-
-//   const taskAddHandler = (task) => {
-//     setTasks((prevTasks) => prevTasks.concat(task));
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <NewTask onAddTask={taskAddHandler} />
-//       <Tasks
-//         items={tasks}
-//         loading={isLoading}
-//         error={error}
-//         onFetch={fetchTasks}
-//       />
-//     </React.Fragment>
-//   );
-// }
-
-// export default App;
-
-// const NewTask = (props) => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-
-//   const enterTaskHandler = async (taskText) => {
-//     setIsLoading(true);
-//     setError(null);
-//     try {
-//       const response = await fetch(
-//         'https://apartments-app-6a66f-default-rtdb.firebaseio.com/tasks.json',
-//         {
-//           method: 'POST',
-//           body: JSON.stringify({ text: taskText }),
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//         }
-//       );
-
-//       if (!response.ok) {
-//         throw new Error('Request failed!');
-//       }
-
-//       const data = await response.json();
-
-//       const generatedId = data.name; // firebase-specific => "name" contains generated id
-//       const createdTask = { id: generatedId, text: taskText };
-
-//       props.onAddTask(createdTask);
-//     } catch (err) {
-//       setError(err.message || 'Something went wrong!');
-//     }
-//     setIsLoading(false);
-//   };
-
-//   return (
-//     <div>
-//       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-//       {error && <p>{error}</p>}
-//     </div>
-//   );
-// };
-
-// const TaskForm = (props) => {
-//   const [task, setTask] = useState('')
-//   const taskInputHandler = e => setTask(e.target.value)
-
-//   const submitHandler = (event) => {
-//     event.preventDefault();
-
-//     if (task.length > 0) {
-//       props.onEnterTask(task);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={submitHandler}>
-//       <input type='text' onChange={taskInputHandler} />
-//       <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
-//     </form>
-//   );
-// };
-
-// const Tasks = (props) => {
-//   let taskList = <h2>No tasks found. Start adding some!</h2>;
-
-//   if (props.items.length > 0) {
-//     taskList = (
-//       <ul>
-//         {props.items.map((task) => (
-//           <li key={task.id}>{task.text}</li>
-//         ))}
-//       </ul>
-//     );
-//   }
-
-//   let content = taskList;
-
-//   if (props.error) {
-//     content = <button onClick={props.onFetch}>Try again</button>;
-//   }
-
-//   if (props.loading) {
-//     content = 'Loading tasks...';
-//   }
-
-//   return (
-//     <div>
-//       <div>{content}</div>
-//     </div>
-//   );
-// };
-
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const setNewTask = task => setTasks(prev => [...prev, task])
+
+  const getTasks = async () => {
+    setIsLoading(true)
+    setError(null)
+
+    try {
+      const response = await fetch('https://apartments-app-6a66f-default-rtdb.firebaseio.com/tasks.json')
+      if (!response.ok) throw new Error('Request failed!');
+      const responseData = await response.json()
+
+      const loadedTasks = []
+
+      for (const key in responseData) {
+        loadedTasks.push({
+          id: key,
+          text: responseData[key].text
+        })
+      }
+
+      setTasks(loadedTasks)
+
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
   return (
     <div>
-      {task}
+      <NewTask onSetNewTask={setNewTask} />
+      { isLoading && <p>Tasks are loading...</p>}
+      { error && <button onClick={getTasks}>{error} Try again!</button> }
+      { tasks.length === 0 && <p>There is no tasks! Add some!</p>}
+      {tasks.map((task, i) => 
+        <div key={i} className='border-2 border-black'>
+          <p>{task.id}</p>
+          <p>{task.text}</p>
+        </div>  
+      )}
     </div>
   )
 }
 
 export default App;
+
+const NewTask = ({ onSetNewTask }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [enteredTask, setEnteredTask] = useState('')
+
+  const enteredTaskChangeHandler = e => setEnteredTask(e.target.value)
+
+  const addTask = async (enteredTask) => {
+    setIsLoading(true)
+    setError(null)
+
+    try {
+      const response = await fetch('https://apartments-app-6a66f-default-rtdb.firebaseio.com/tasks.json', {
+        method: 'POST',
+        body: JSON.stringify({ text: enteredTask }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      const responseData = await response.json()
+      
+      const firebaseId = responseData.name
+      onSetNewTask({ id: firebaseId, text: enteredTask })
+      
+    } catch (error) {
+      setError(error)
+    }
+
+    setIsLoading(false)
+  }
+
+  const submitFormHandler = e => {
+    e.preventDefault()
+
+    if (enteredTask.length > 0) addTask(enteredTask)
+  }
+
+  if (error) return <p>{error}</p>
+
+  return (
+    <form onSubmit={submitFormHandler}>
+      <input value={enteredTask} onChange={enteredTaskChangeHandler} type="text" placeholder="Enter new task..." />
+      <button>{ isLoading ? <p>Sending...</p> : <p>Submit task</p>}</button>
+    </form>
+  )
+}
